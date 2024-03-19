6d0c031366ae909e82f3628a5c3c0e4f369de2f1
MrKeviscool <danielandkev@outlook.com>
2024-03-20 01:36:07 +1030

    version 1 of:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clearscreen::{self, clear};
-use std::io::{Write, Read};
+use std::io::{Read, Write};
 
 struct ListObject{
     name:String,
@@ -12,7 +12,7 @@ fn main() {
     loop{
         displaylist(&todoos);
         let mut inputbuffer:String = String::new();
-        println!("\n\n[A]dd [D]isplay-content [S]crap");
+        println!("\n\n[A]dd [S]crap [D]isplay-content [F]inish-task");
         print!("command: ");
         std::io::stdout().flush().unwrap();
         std::io::stdin().read_line(&mut inputbuffer).unwrap();
@@ -26,6 +26,9 @@ fn main() {
         }
         else if inputbuffer == 's'{
             removeeliment(&mut todoos);
+        }
+        else if inputbuffer == 'f'{
+            togglecompletion(&mut todoos);
         }
     }
 }
@@ -80,15 +83,54 @@ fn showcontents(todoos: &Vec<ListObject>){
 fn removeeliment(todoos: &mut Vec<ListObject>){
     displaylist(todoos);
     let mut inputbuffer: String = String::new();
-    print!("\nremove ID: ");
+    print!("\nA for all. D for all done.   remove ID: ");
     std::io::stdout().flush().unwrap();
     std::io::stdin().read_line(&mut inputbuffer).unwrap();
+    if inputbuffer.chars().nth(0).unwrap().to_ascii_lowercase() == 'a'
+    {
+        todoos.clear();
+        return;
+    }
+    else if inputbuffer.chars().nth(0).unwrap().to_ascii_lowercase() == 'd'{
+        for i in (0..todoos.len()).rev(){
+            if todoos[i].completed == true {
+                todoos.remove(i);
+            }
+        }
+    }
     let inputbuffer:u8 = match inputbuffer.trim().parse(){
         Ok(num) => num,
         Err(_) => return,
     };
-    if inputbuffer > todoos.len() as u8{
+    if inputbuffer > (todoos.len()-1) as u8{
         return;
     }
     todoos.remove(inputbuffer as usize);
+}
+
+fn togglecompletion(todoos: &mut Vec<ListObject>){
+    displaylist(todoos);
+    let mut inputbuffer: String = String::new();
+    print!("\nA for all.  complete ID: ");
+    std::io::stdout().flush().unwrap();
+    std::io::stdin().read_line(&mut inputbuffer).unwrap();
+    if inputbuffer.chars().nth(0).unwrap().to_ascii_lowercase() == 'a'{
+        for i in 0..todoos.len(){
+            todoos[i].completed = true;
+        }
+        return;
+    }
+    let inputbuffer:u8 = match inputbuffer.trim().parse(){
+        Ok(num) => num,
+        Err(_) => return,
+    };
+    if inputbuffer > (todoos.len()-1) as u8{
+        return;
+    }
+    if todoos[inputbuffer as usize].completed == true{
+        todoos[inputbuffer as usize].completed = false;
+    }
+    else{
+        todoos[inputbuffer as usize].completed = true;
+    }
 }
